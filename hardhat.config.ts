d3ea6053478a6917f032b38e5d5364e2ca0d961c
peebaKU <baramee.254450@gmail.com>
2025-03-12 18:24:03 +0700

    edit version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,81 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
+import "hardhat-contract-sizer";
+import "dotenv/config";
+
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY";
+
+const POLYGON_MAINNET_RPC_URL =
+    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key";
+
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "0x";
+const MNEMONIC: string = process.env.MNEMONIC || "your mnemonic";
+
+const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
+const POLYGONSCAN_API_KEY: string = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key";
+const REPORT_GAS: boolean = process.env.REPORT_GAS === "true";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: PRIVATE_KEY !== "0x" ? [PRIVATE_KEY] : [],
+            chainId: 11155111,
+        },
+        polygon: {
+            url: POLYGON_MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== "0x" ? [PRIVATE_KEY] : [],
+            chainId: 137,
+        },
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["NftMarketplace"],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
+    mocha: {
+        timeout: 200000,
+    },
 };
 
 export default config;
